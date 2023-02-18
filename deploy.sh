60314327c15e72d9b3a613a38f5b73a1d5735256
OldBrain <hell_brain@mail.ru>
2023-02-18 22:33:27 +0400

    Порт 80. Подготовка для публикации на сервер.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
 mvn clean packed
+ Стартуем на сервере
+ java -jar fast-pipe.jar >log.txt
--- src/main/java/ru/react/fastpipe/configuration/WebConfig.java
+++ src/main/java/ru/react/fastpipe/configuration/WebConfig.java
@@ -1,7 +1,9 @@
 package ru.react.fastpipe.configuration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.reactive.config.PathMatchConfigurer;
@@ -9,8 +11,10 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
 //@EnableWebFlux
-@AllArgsConstructor
+//@AllArgsConstructor
 public class WebConfig implements WebFluxConfigurer {
+    @Value("${hostname}")
+    private String hostName;
     @Override
     public void configurePathMatching(PathMatchConfigurer configurer) {
         configurer
